689be618e668d3fbf64a6bb4c53a09cb131488f7
Rafael Dantas <rafael.dantas2001@hotmail.com>
2019-08-09 22:38:56 -0300

    exercicio02

---
+++ exercicio2.ts
@@ -0,0 +1,19 @@
+let nota : number = 87;
+
+if (nota == 0) {
+    console.log("E")
+} else if (nota > 1  && nota <= 35 ){
+    console.log("D")
+}
+
+else if (nota > 36  && nota < 60 ){
+    console.log("C")
+}
+
+else if (nota > 61  && nota < 85 ){
+    console.log("B")
+}
+
+else  {
+    console.log("A")
+}
